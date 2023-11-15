331184a395ab75851c8c85e0a1db8a0e7b95d6ce
iswariya i <145429803+iswariya27@users.noreply.github.com>
2023-11-15 15:34:10 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,22 +4,74 @@ import "../styles/navbar.css";
 
 const Navbar = ({ setShow, size }) => {
     return (
-        <nav>
-      <div className="nav_box">
-        <span className="my_shop" onClick={() => setShow(true)}>
-          My Shoping
-        </span>
-        <span className="my_shop" onClick={() => setShow(true)}>
-          My Shoping
-        </span>
-        <div className="cart" onClick={() => setShow(false)}>
-          <span>
-            <i className="fas fa-cart-plus"></i>
-          </span>
-          <span>{size}</span>
-        </div>
-      </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container px-4 px-lg-5">
+                <a className="navbar-brand" href="/">
+                    Start Bootstrap
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                id="navbarDropdown"
+                                href="/"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Shop
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        All Products
+                                    </a>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider" />
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        Popular Items
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        New Arrivals
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form className="cart" onClick={() => setShow(false)}>
+                        <span>
+                            <i className="fas fa-cart-plus"></i>
+                        </span>
+                        <span>{size}</span>
+                    </form>
+                </div>
+            </div>
+        </nav>
     );
 }
 
